87fcf5f8c7fd662ef728f5f115bc85857ab3765e
milansoap <milan.seid123@gmail.com>
2023-08-31 00:37:31 +0200

    navbar

---
+++ src/app/components/navbar/Navbar.tsx
@@ -0,0 +1,31 @@
+import Container from "../Container";
+import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
+
+const Navbar = () => {
+    return (
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+            <div className="py-4 border-b-[1px]">
+                <Container children={
+                    <div className="
+                    flex
+                    flex-grow
+                    items-center
+                    justify-between
+                    gap-3
+                    md:gap-0
+                    "
+                    >
+                        <Logo />
+                        <Search />
+                        <UserMenu />
+                        </div>
+                } />
+                
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <div>Hello Airbnb</div>
+    <div className='text-rose-500 text-4xl'>Hello Airbnb</div>
   )
 }
